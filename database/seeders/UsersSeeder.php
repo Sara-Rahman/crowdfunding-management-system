2be3c84565137022c450920bf1418af6f0f33232
Sifat-1 <83138287+Sifat-1@users.noreply.github.com>
2022-03-08 02:35:27 +0600

    admin authentication seeder has been created

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       $role= Role::create (
+            [
+                'name'=>'admin',
+                'slug'=>'login of admin',
+            ]
+
+            );
+
+            
+        User::create(
+            [
+                'role_id'=>$role->id,
+                'name'=>'Nowshin',
+                'email'=>'nowshin1@gmail.com',
+                'password'=>bcrypt('12345'),
+                'mobile'=>'01727265497',
+                'role'=>'admin'
+              
+            ]
+        );
+    }
+}
